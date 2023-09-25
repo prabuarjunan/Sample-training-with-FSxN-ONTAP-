a665260f040c4939d5f0cd181c685dc523468859
Prabu Arjunan <31085459+prabuarjunan@users.noreply.github.com>
2023-09-25 15:55:41 -0700

    create_example.py

---
+++ example.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load your data from the CSV file
+data = pd.read_csv('data.csv')
+
+# Assuming your CSV contains a 'target' column for classification
+# Replace 'target' with your actual target column name
+X = data.drop(columns=['target'])
+y = data['target']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train a RandomForestClassifier
+clf = RandomForestClassifier(random_state=42)
+clf.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = clf.predict(X_test)
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Model Accuracy: {accuracy}")
